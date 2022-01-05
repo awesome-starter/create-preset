3073e543050c6ab2c8650e33122b0aa2647fceca
chengpeiquan <chengpeiquan@chengpeiquan.com>
2022-01-05 16:38:45 +0800

    chore: save the coding progress

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 13,
     sourceType: 'module',
-    parser: '@babel/eslint-parser',
+    // parser: '@babel/eslint-parser',
     requireConfigFile: false,
     allowImportExportEverywhere: true,
   },
