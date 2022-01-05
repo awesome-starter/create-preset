911f0c5738b2cd48c37f6832d7b6a71ed28ef9d0
chengpeiquan <chengpeiquan@chengpeiquan.com>
2022-01-05 16:30:40 +0800

    chore: save the coding progress

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'error',
     'prettier/prettier': 'warn',
   },
 }
